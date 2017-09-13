efc9e99a6b88751747e885985f1ed73d06fd09fb
nakeyxie <xiechengyu5@163.com>
2017-09-13 22:19:28 +0800

    把test提交到本地仓库

---
+++ src/testjdbc/DealReult.java
@@ -0,0 +1,10 @@
+package testjdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface DealReult {
+	
+	public Object handleRult (ResultSet rs)throws SQLException ;
+
+}
